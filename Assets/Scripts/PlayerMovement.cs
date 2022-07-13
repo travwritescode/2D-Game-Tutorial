2b612b84ab71cd84c74268c71badc30cddc2337c
Travis <travis.anderson30@gmail.com>
2022-07-13 09:23:55 -0600

    Added jumping and falling animations to the player character and added collision detection with the ground to prevent jumping into space. Also adjusted terrain so we can't jump off the sides of it.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private BoxCollider2D coll;
     private Animator animator;
     private SpriteRenderer spriteRenderer;
+
+    [SerializeField] private LayerMask jumpableGround;
 
     private float dirX;
     //SerializeField exposes these fields to the Unity editor
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 14f;
 
+    private enum MovementState { idle, running, jumping, falling };
+
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
@@ -27,7 +33,7 @@ public class PlayerMovement : MonoBehaviour
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(moveSpeed * dirX, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
@@ -37,20 +43,39 @@ public class PlayerMovement : MonoBehaviour
 
     private void UpdateAnimationState(float dirX)
     {
+        MovementState state;
+
         if (dirX > 0f)
         {
-            animator.SetBool("running", true);
+            state = MovementState.running;
             spriteRenderer.flipX = false;
         }
         else if (dirX < 0f)
         {
-            animator.SetBool("running", true);
+            state = MovementState.running;
             spriteRenderer.flipX = true;
         }
         else
         {
-            animator.SetBool("running", false);
+            state = MovementState.idle;
         }
 
+        if (rb.velocity.y > .1f)
+        {
+            state = MovementState.jumping;
+        } else if (rb.velocity.y < -.1f)
+        {
+            state = MovementState.falling;
+        }
+
+        animator.SetInteger("state", (int)state);
+    }
+
+    // This is basically a box we created in code and shifted down by .1f
+    // then we check for the overlap with the ground to see that we are colliding
+    // and can jump from the ground
+    private bool IsGrounded() 
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
